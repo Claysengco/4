70d09ca78cfe1f9435f7a1b0ca288a02e5058259
Claysengco <135989421+Claysengco@users.noreply.github.com>
2023-06-23 10:07:54 +0800

    Add files via upload

---
+++ A.T.R_ver 2/main.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".bg-slider-thumbs", {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
+  });
+  var swiper2 = new Swiper(".bg-slider", {
+    loop: true,
+    spaceBetween: 0,
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+
+Window.addEventListener("scroll", function () { 
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
